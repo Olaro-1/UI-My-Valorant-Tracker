39c55aaf903aafcd2cc554f71ddb78a08e6030b3
Olaro <irfanmaulanafalah@gmail.com>
2022-10-30 01:46:51 +0700

    This is my UI task

---
+++ app/src/main/java/exportkit/figma/homepage_activity.java
@@ -0,0 +1,166 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		loadingpage
+	 *	@date 		Tuesday 25th of October 2022 02:25:16 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class homepage_activity extends Activity {
+
+	
+	private View _bg__homepage_ek2;
+	private View rectangle_1;
+	private View rectangle_3;
+	private View rectangle_2;
+	private ImageView this_1;
+	private View rectangle_4;
+	private ImageView union;
+	private TextView add_your_account;
+	private View kolom_find_player;
+	private ImageView shape;
+	private TextView find_player;
+	private ImageView player_card_viper_1;
+	private TextView vrx_olaro;
+	private TextView diamond_2;
+	private ImageView _336460da_b54e_4dbf_bd38_7fe2c0b90362_removebg_preview_1;
+	private View kolom_agent;
+	private TextView persentase_viper;
+	private TextView kda_viper;
+	private ImageView viper_avatar_1;
+	private ImageView astra_icon_1;
+	private TextView persentase_astra;
+	private TextView kda_astra;
+	private TextView persentase_viper_ek1;
+	private TextView kda_viper_ek1;
+	private ImageView omen_avatar_1;
+	private TextView _30w_10l_75__kda_2_1;
+	private TextView your_role_seems_to_controller_;
+	private View rectangle_5;
+	private ImageView shape_ek1;
+	private TextView search;
+	private TextView agents_ek1;
+	private ImageView shape_ek2;
+	private TextView ranking_ek1;
+	private ImageView shape_ek3;
+	private TextView my_profile_ek1;
+	private ImageView shape_ek4;
+	private View rectangle_6;
+	private View rectangle_7;
+	private TextView w___35_51;
+	private TextView _26_14_8;
+	private TextView _2_43;
+	private ImageView jett_avatar_1;
+	private TextView _13_10;
+	private TextView on_bind_3_days_ago;
+	private View rectangle_6_ek1;
+	private View rectangle_7_ek1;
+	private TextView w___31_10;
+	private TextView _19_11_13;
+	private TextView _2_9;
+	private TextView _13_9;
+	private TextView on_breeze_3_days_ago;
+	private ImageView viper_avatar_2;
+	private View rectangle_6_ek2;
+	private View rectangle_7_ek2;
+	private TextView w___43_57;
+	private TextView _21_13_15;
+	private TextView _2_26;
+	private TextView _14_12;
+	private TextView on_ascent_3_days_ago;
+	private ImageView astra_icon_2;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.homepage);
+
+		
+		_bg__homepage_ek2 = (View) findViewById(R.id._bg__homepage_ek2);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		this_1 = (ImageView) findViewById(R.id.this_1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		union = (ImageView) findViewById(R.id.union);
+		add_your_account = (TextView) findViewById(R.id.add_your_account);
+		kolom_find_player = (View) findViewById(R.id.kolom_find_player);
+		shape = (ImageView) findViewById(R.id.shape);
+		find_player = (TextView) findViewById(R.id.find_player);
+		player_card_viper_1 = (ImageView) findViewById(R.id.player_card_viper_1);
+		vrx_olaro = (TextView) findViewById(R.id.vrx_olaro);
+		diamond_2 = (TextView) findViewById(R.id.diamond_2);
+		_336460da_b54e_4dbf_bd38_7fe2c0b90362_removebg_preview_1 = (ImageView) findViewById(R.id._336460da_b54e_4dbf_bd38_7fe2c0b90362_removebg_preview_1);
+		kolom_agent = (View) findViewById(R.id.kolom_agent);
+		persentase_viper = (TextView) findViewById(R.id.persentase_viper);
+		kda_viper = (TextView) findViewById(R.id.kda_viper);
+		viper_avatar_1 = (ImageView) findViewById(R.id.viper_avatar_1);
+		astra_icon_1 = (ImageView) findViewById(R.id.astra_icon_1);
+		persentase_astra = (TextView) findViewById(R.id.persentase_astra);
+		kda_astra = (TextView) findViewById(R.id.kda_astra);
+		persentase_viper_ek1 = (TextView) findViewById(R.id.persentase_viper_ek1);
+		kda_viper_ek1 = (TextView) findViewById(R.id.kda_viper_ek1);
+		omen_avatar_1 = (ImageView) findViewById(R.id.omen_avatar_1);
+		_30w_10l_75__kda_2_1 = (TextView) findViewById(R.id._30w_10l_75__kda_2_1);
+		your_role_seems_to_controller_ = (TextView) findViewById(R.id.your_role_seems_to_controller_);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		shape_ek1 = (ImageView) findViewById(R.id.shape_ek1);
+		search = (TextView) findViewById(R.id.search);
+		agents_ek1 = (TextView) findViewById(R.id.agents_ek1);
+		shape_ek2 = (ImageView) findViewById(R.id.shape_ek2);
+		ranking_ek1 = (TextView) findViewById(R.id.ranking_ek1);
+		shape_ek3 = (ImageView) findViewById(R.id.shape_ek3);
+		my_profile_ek1 = (TextView) findViewById(R.id.my_profile_ek1);
+		shape_ek4 = (ImageView) findViewById(R.id.shape_ek4);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		rectangle_7 = (View) findViewById(R.id.rectangle_7);
+		w___35_51 = (TextView) findViewById(R.id.w___35_51);
+		_26_14_8 = (TextView) findViewById(R.id._26_14_8);
+		_2_43 = (TextView) findViewById(R.id._2_43);
+		jett_avatar_1 = (ImageView) findViewById(R.id.jett_avatar_1);
+		_13_10 = (TextView) findViewById(R.id._13_10);
+		on_bind_3_days_ago = (TextView) findViewById(R.id.on_bind_3_days_ago);
+		rectangle_6_ek1 = (View) findViewById(R.id.rectangle_6_ek1);
+		rectangle_7_ek1 = (View) findViewById(R.id.rectangle_7_ek1);
+		w___31_10 = (TextView) findViewById(R.id.w___31_10);
+		_19_11_13 = (TextView) findViewById(R.id._19_11_13);
+		_2_9 = (TextView) findViewById(R.id._2_9);
+		_13_9 = (TextView) findViewById(R.id._13_9);
+		on_breeze_3_days_ago = (TextView) findViewById(R.id.on_breeze_3_days_ago);
+		viper_avatar_2 = (ImageView) findViewById(R.id.viper_avatar_2);
+		rectangle_6_ek2 = (View) findViewById(R.id.rectangle_6_ek2);
+		rectangle_7_ek2 = (View) findViewById(R.id.rectangle_7_ek2);
+		w___43_57 = (TextView) findViewById(R.id.w___43_57);
+		_21_13_15 = (TextView) findViewById(R.id._21_13_15);
+		_2_26 = (TextView) findViewById(R.id._2_26);
+		_14_12 = (TextView) findViewById(R.id._14_12);
+		on_ascent_3_days_ago = (TextView) findViewById(R.id.on_ascent_3_days_ago);
+		astra_icon_2 = (ImageView) findViewById(R.id.astra_icon_2);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/exportkit/figma/loadingpage_activity.java
@@ -0,0 +1,49 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		loadingpage
+	 *	@date 		Tuesday 25th of October 2022 02:25:16 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class loadingpage_activity extends Activity {
+
+	
+	private View _bg__loadingpage_ek2;
+	private ImageView no_background_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loadingpage);
+
+		
+		_bg__loadingpage_ek2 = (View) findViewById(R.id._bg__loadingpage_ek2);
+		no_background_1 = (ImageView) findViewById(R.id.no_background_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
